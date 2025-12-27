c9750291b9fafc8280d6cbd546cdfa7f66019542
Neeraj708888 <amck.sharma@gmail.com>
2025-12-27 21:39:37 +0530

    Find any largest number given position

---
+++ ARRAY_Problems/findAnyLargestElement.js
@@ -0,0 +1,31 @@
+// Find Any Largest Element In An Array O(n * k)
+
+function findAnyLargest (arr, position) {
+  let largest;
+  
+  for (let i = 1; i <= position; i++) {
+    
+    largest = -Infinity;
+    
+    // Find current largest number
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > largest) {
+        
+        largest = arr[j];
+      }
+    }
+    
+    // Remove that largest number found in current iteration
+   for (let k = 0; k < arr.length; k++) {
+      if (arr[k] === largest) {
+      arr[k] = -Infinity;
+      break;
+    }
+   }
+  }
+  
+  return largest;
+}
+
+console.log(findAnyLargest([12,4,13,6,10,14], 1));
+// [4,6,10,12,13,14]
